a523005f9b89e34ed2e4292a09a11e7d9be846e1
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-11-13 17:40:26 +0700

    init template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'data-quality';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    require('../../src/assets/dashforge/assets/js/dashforge.aside.js');
+  }
 }
